19454e3381e489b9d9b8664f3a240d5990ceb058
bwangfdu <bwang.fdu@gmail.com>
2015-01-21 12:02:58 +0800

    格式化代码

--- weixin/src/main/java/edu/fudan/weixin/actions/BindAction.java
+++ weixin/src/main/java/edu/fudan/weixin/actions/BindAction.java
@@ -47,15 +47,23 @@ public class BindAction extends GuestActionBase {
 	@Action("bindin")
 	public String input() {
 		Config conf = Config.getInstance();
-		byte[] bs=new byte[16];
+		byte[] bs = new byte[16];
 		new Random().nextBytes(bs);
-		String st =EncodeHelper.bytes2hex(bs) ;			
+		String st = EncodeHelper.bytes2hex(bs);
 		// 放进一个使用EhCache维护的容器，当用户从微信的OAuth2.0拿到code后检查这个链接是不是由此链接生成的。
-		//CacheManager.getInstance().getCache("WXStates").put(new Element(String.valueOf(st), st));
-		//使用Session检查st的合法性
+		// CacheManager.getInstance().getCache("WXStates").put(new
+		// Element(String.valueOf(st), st));
+		// 使用Session检查st的合法性
 		getSession().put("wxstate", st);
 		try {
-			org.apache.struts2.ServletActionContext.getResponse().sendRedirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + conf.get("weixin.appid") + "&redirect_uri=" + URLEncoder.encode(conf.get("weixin.context")+"uisbind.act", "utf-8") + "&response_type=code&scope=snsapi_base&state=" + st + "#wechat_redirect");
+			org.apache.struts2.ServletActionContext.getResponse().sendRedirect(
+					"https://open.weixin.qq.com/connect/oauth2/authorize?appid="
+							+ conf.get("weixin.appid")
+							+ "&redirect_uri="
+							+ URLEncoder.encode(conf.get("weixin.context")
+									+ "uisbind.act", "utf-8")
+							+ "&response_type=code&scope=snsapi_base&state="
+							+ st + "#wechat_redirect");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -67,11 +75,18 @@ public class BindAction extends GuestActionBase {
 	@Action("uisbind")
 	public String uis() {
 
-		if (!CommonUtil.isEmpty(code)&&!CommonUtil.isEmpty(state)&&state.equals(getSession().remove("wxstate"))) {
+		if (!CommonUtil.isEmpty(code) && !CommonUtil.isEmpty(state)
+				&& state.equals(getSession().remove("wxstate"))) {
 
 			Config conf = Config.getInstance();
 			// 获取微信的access_token
-			String urlstr = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + conf.get("weixin.appid") + "&secret=" + conf.get("weixin.secret") + "&code=" + code + "&grant_type=authorization_code";
+			String urlstr = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="
+					+ conf.get("weixin.appid")
+					+ "&secret="
+					+ conf.get("weixin.secret")
+					+ "&code="
+					+ code
+					+ "&grant_type=authorization_code";
 			try {
 				String ret = CommonUtil.getWebContent(urlstr).toString();
 				DBObject retobj = (DBObject) JSON.parse(ret);
@@ -80,13 +95,17 @@ public class BindAction extends GuestActionBase {
 					Object openid = retobj.get("openid");
 					// 设置Session
 					getSession().put("openid", openid);
-					DBCollection c = MongoUtil.getInstance().getDB().getCollection("Bindings");
-					DBObject obj = c.findOne(new BasicDBObject("openid", openid));
+					DBCollection c = MongoUtil.getInstance().getDB()
+							.getCollection("Bindings");
+					DBObject obj = c
+							.findOne(new BasicDBObject("openid", openid));
 					if (CommonUtil.isEmpty(obj)) {
-						obj = new BasicDBObject().append("openid", retobj.get("openid"));
+						obj = new BasicDBObject().append("openid",
+								retobj.get("openid"));
 					}
 					obj.put("weixintoken", acctk);
-					obj.put("weixinexpired", System.currentTimeMillis() + 1000 * (int) retobj.get("expires_in"));
+					obj.put("weixinexpired", System.currentTimeMillis() + 1000
+							* (int) retobj.get("expires_in"));
 					obj.put("weixinscope", retobj.get("scope"));
 					obj.put("wexinrefresh", retobj.get("refresh_token"));
 					c.save(obj);
@@ -96,11 +115,36 @@ public class BindAction extends GuestActionBase {
 					// addActionMessage("UIS已经绑定为" + userinfo.get("user_id"));
 					// } else {
 					// 重定向到复旦TAC的授权页面
-					org.apache.struts2.ServletActionContext.getResponse().sendRedirect(conf.get("tac.codeurl")+"?response_type=code&scope=" + URLEncoder.encode(conf.get("tac.scope"), "utf-8") + "&client_id=" + conf.get("tac.clientid") + "&redirect_uri=" + URLEncoder.encode(conf.get("tac.redirecturi"), "utf-8") + "&state=" + EncodeHelper.bytes2hex(EncodeHelper.encrypt("AES", obj.get("openid").toString().getBytes(), EncodeHelper.hex2bytes(conf.get("tac.enckey")), null)));
+					org.apache.struts2.ServletActionContext
+							.getResponse()
+							.sendRedirect(
+									conf.get("tac.codeurl")
+											+ "?response_type=code&scope="
+											+ URLEncoder.encode(
+													conf.get("tac.scope"),
+													"utf-8")
+											+ "&client_id="
+											+ conf.get("tac.clientid")
+											+ "&redirect_uri="
+											+ URLEncoder.encode(
+													conf.get("tac.redirecturi"),
+													"utf-8")
+											+ "&state="
+											+ EncodeHelper.bytes2hex(EncodeHelper
+													.encrypt(
+															"AES",
+															obj.get("openid")
+																	.toString()
+																	.getBytes(),
+															EncodeHelper
+																	.hex2bytes(conf
+																			.get("tac.enckey")),
+															null)));
 					return NONE;
 					// }
 				} else {
-					addActionError("Error in Weixin OAuth2 : " + retobj.get("errcode") + retobj.get("errmsg"));
+					addActionError("Error in Weixin OAuth2 : "
+							+ retobj.get("errcode") + retobj.get("errmsg"));
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -123,73 +167,90 @@ public class BindAction extends GuestActionBase {
 			Map<String, Object> retobj = OAuth2Helper.getToken(code, false);
 			Object acctk = retobj.get("access_token");
 			if (!CommonUtil.isEmpty(acctk)) {
-				DBCollection c = MongoUtil.getInstance().getDB().getCollection("Bindings");
-				String openid = new String(EncodeHelper.dencrypt("AES", EncodeHelper.hex2bytes(state), EncodeHelper.hex2bytes(conf.get("tac.enckey")), null));
-				if(!CommonUtil.isEmpty(openid)&&openid.equals(getSession().get("openid"))){
-				DBObject idobj = c.findOne(new BasicDBObject("openid", openid));
-				if (!CommonUtil.isEmpty(idobj)) {
-					BasicDBObject obj = new BasicDBObject();
-					obj.put("uistoken", acctk);
-					obj.put("uisexpired", System.currentTimeMillis() + 1000 * (int) retobj.get("expires_in"));
-					obj.put("uisscope", scope);
-					obj.put("uisrefresh", retobj.get("refresh_token"));
-					// 获取用户信息
+				DBCollection c = MongoUtil.getInstance().getDB()
+						.getCollection("Bindings");
+				String openid = new String(EncodeHelper.dencrypt("AES",
+						EncodeHelper.hex2bytes(state),
+						EncodeHelper.hex2bytes(conf.get("tac.enckey")), null));
+				if (!CommonUtil.isEmpty(openid)
+						&& openid.equals(getSession().get("openid"))) {
+					DBObject idobj = c.findOne(new BasicDBObject("openid",
+							openid));
+					if (!CommonUtil.isEmpty(idobj)) {
+						BasicDBObject obj = new BasicDBObject();
+						obj.put("uistoken", acctk);
+						obj.put("uisexpired", System.currentTimeMillis() + 1000
+								* (int) retobj.get("expires_in"));
+						obj.put("uisscope", scope);
+						obj.put("uisrefresh", retobj.get("refresh_token"));
+						// 获取用户信息
 
-					retobj = TACOAuth2Helper.fetchUser(String.valueOf(acctk));
-					if (!CommonUtil.isEmpty(retobj.get("user_id"))) {
-						Object email = retobj.get("email");
-						Object userid = retobj.get("user_id");
-						Object bds = idobj.get("binds");
-						if (!CommonUtil.isEmpty(bds) && bds instanceof List) {
-							Iterator i = ((List) bds).iterator();
-							while (i.hasNext()) {
-								Object o = i.next();
-								if (!CommonUtil.isEmpty(o)) {
-									DBObject bd = (DBObject) o;
-									if (CommonUtil.eq(userid, bd.get("uisid")))
+						retobj = TACOAuth2Helper.fetchUser(String
+								.valueOf(acctk));
+						if (!CommonUtil.isEmpty(retobj.get("user_id"))) {
+							Object email = retobj.get("email");
+							Object userid = retobj.get("user_id");
+							Object bds = idobj.get("binds");
+							if (!CommonUtil.isEmpty(bds) && bds instanceof List) {
+								Iterator i = ((List) bds).iterator();
+								while (i.hasNext()) {
+									Object o = i.next();
+									if (!CommonUtil.isEmpty(o)) {
+										DBObject bd = (DBObject) o;
+										if (CommonUtil.eq(userid,
+												bd.get("uisid")))
+											i.remove();
+									} else
 										i.remove();
-								} else
-									i.remove();
+								}
+
+							} else {
+								bds = new BasicDBList();
+								idobj.put("binds", bds);
+							}
+							obj.put("uisid", userid);
+							obj.put("email", email);
+							obj.put("username", retobj.get("user_name"));
+							obj.put("usertype", retobj.get("user_type"));
+							((List) bds).add(obj);
+							addActionMessage("成功将此微信号与" + retobj.get("user_id")
+									+ "完成了绑定");
+							// 如果可以获取到Email发个通知
+							if (String.valueOf(userid).matches("\\d{7,}"))
+								email = userid + "@fudan.edu.cn";
+							if (!CommonUtil.isEmpty(email)) {
+								try {
+
+									SMTP smtp = new ServiceMail();
+									smtp.addTo(email.toString());
+									smtp.setTitle("微信账号与UIS账号绑定成功");
+									String mb = conf.get("bind.mail");
+									mb = mb.replaceAll("%uis%", String
+											.valueOf(retobj.get("user_id")));
+									DBObject user = MongoUtil
+											.getInstance()
+											.getDB()
+											.getCollection("weixinuser")
+											.findOne(
+													new BasicDBObject("openid",
+															openid));
+									if (!CommonUtil.isEmpty(user))
+										mb = mb.replaceAll("%nickname%", String
+												.valueOf(user.get("nickname")));
+									smtp.setMailbody(mb, false);
+									smtp.send();
+								} catch (Exception e) {
+									addActionError("Error in Send Mail:" + e);
+								}
 							}
 
 						} else {
-							bds = new BasicDBList();
-							idobj.put("binds", bds);
+							addActionError("Error in TAC Resource:"
+									+ retobj.get("error"));
 						}
-						obj.put("uisid", userid);
-						obj.put("email", email);
-						obj.put("username", retobj.get("user_name"));
-						obj.put("usertype", retobj.get("user_type"));
-						((List) bds).add(obj);
-						addActionMessage("成功将此微信号与" + retobj.get("user_id") + "完成了绑定");
-						// 如果可以获取到Email发个通知
-						if (String.valueOf(userid).matches("\\d{7,}"))
-							email = userid + "@fudan.edu.cn";
-						if (!CommonUtil.isEmpty(email)) {
-							try {
-
-								SMTP smtp = new ServiceMail();
-								smtp.addTo(email.toString());
-								smtp.setTitle("微信账号与UIS账号绑定成功");
-								String mb = conf.get("bind.mail");
-								mb = mb.replaceAll("%uis%", String.valueOf(retobj.get("user_id")));
-								DBObject user = MongoUtil.getInstance().getDB().getCollection("weixinuser").findOne(new BasicDBObject("openid", openid));
-								if (!CommonUtil.isEmpty(user))
-									mb = mb.replaceAll("%nickname%", String.valueOf(user.get("nickname")));
-								smtp.setMailbody(mb, false);
-								smtp.send();
-							} catch (Exception e) {
-								addActionError("Error in Send Mail:" + e);
-							}
-						}
-
-					} else {
-						addActionError("Error in TAC Resource:" + retobj.get("error"));
+						c.save(idobj);
 					}
-					c.save(idobj);
-				}
-				}else
-				{
+				} else {
 					addActionError("该绑定请求的来源中的微信账号信息与当前账号不匹配，可能存在安全风险，请通过“复旦信息办”微信公众账号获取最新的绑定链接！");
 				}
 			} else {
--- weixin/src/main/java/edu/fudan/weixin/actions/WeixinLoginAction.java
+++ weixin/src/main/java/edu/fudan/weixin/actions/WeixinLoginAction.java
@@ -39,19 +39,19 @@ public class WeixinLoginAction extends GuestActionBase {
 		if (CommonUtil.isEmpty(getSession().get("openid"))) {
 			Config conf = Config.getInstance();
 			try {
-				byte[] bs=new byte[16];
+				byte[] bs = new byte[16];
 				new Random().nextBytes(bs);
-				String st =EncodeHelper.bytes2hex(bs) ;				
+				String st = EncodeHelper.bytes2hex(bs);
 				// 放进一个使用EhCache维护的容器，当用户从微信的OAuth2.0拿到code后检查这个链接是不是由此链接生成的。
-				//CacheManager.getInstance().getCache("WXStates")
-				//		.put(new Element(st, redir));
+				// CacheManager.getInstance().getCache("WXStates")
+				// .put(new Element(st, redir));
 				getSession().put("wxstate", st);
 				getSession().put("redir", redir);
 				redir = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="
 						+ conf.get("weixin.appid")
 						+ "&redirect_uri="
-						+ EncodeHelper
-								.encode(conf.get("weixin.context")+"wxlogindo.act", "URL")
+						+ EncodeHelper.encode(conf.get("weixin.context")
+								+ "wxlogindo.act", "URL")
 						+ "&response_type=code&scope=snsapi_base&state="
 						+ st
 						+ "#wechat_redirect";
@@ -71,12 +71,13 @@ public class WeixinLoginAction extends GuestActionBase {
 
 	@Action("wxlogindo")
 	public String logindo() {
-		//Cache cache= CacheManager.getInstance().getCache("WXStates");
-		//Element el=cache.get(state);
-		if (!CommonUtil.isEmpty(code)&&!CommonUtil.isEmpty(state)&&state.equals(getSession().remove("wxstate"))) {
+		// Cache cache= CacheManager.getInstance().getCache("WXStates");
+		// Element el=cache.get(state);
+		if (!CommonUtil.isEmpty(code) && !CommonUtil.isEmpty(state)
+				&& state.equals(getSession().remove("wxstate"))) {
 
-			redir=String.valueOf(getSession().remove("redir"));
-			//cache.removeElement(el);
+			redir = String.valueOf(getSession().remove("redir"));
+			// cache.removeElement(el);
 			Config conf = Config.getInstance();
 			// 获取微信的access_token
 			String urlstr = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="
@@ -110,25 +111,21 @@ public class WeixinLoginAction extends GuestActionBase {
 					obj.put("wexinrefresh", retobj.get("refresh_token"));
 					c.save(obj);
 					/*
-					if(obj.get("binds")!=null &&obj.get("binds") instanceof List)
-					{
-						List<BasicDBObject> ls=new ArrayList<BasicDBObject>();
-						for(DBObject ob:(List<DBObject>)obj.get("binds"))
-						{
-							if(!CommonUtil.isEmpty(ob)){							
-							BasicDBObject bdo=new BasicDBObject();
-							bdo.put("uisid", ob.get("uisid"));
-							bdo.put("username",ob.get("username"));
-							bdo.put("usertype", ob.get("usertype"));
-							ls.add(bdo);
-							}
-						}
-						getSession().put("binds", ls);
-					}
-					*/
-					
-					DBObject user=MongoUtil.getInstance().getDB().getCollection("weixinuser").findOne(new BasicDBObject("openid",openid));
-					if(!CommonUtil.isEmpty(user))
+					 * if(obj.get("binds")!=null &&obj.get("binds") instanceof
+					 * List) { List<BasicDBObject> ls=new
+					 * ArrayList<BasicDBObject>(); for(DBObject
+					 * ob:(List<DBObject>)obj.get("binds")) {
+					 * if(!CommonUtil.isEmpty(ob)){ BasicDBObject bdo=new
+					 * BasicDBObject(); bdo.put("uisid", ob.get("uisid"));
+					 * bdo.put("username",ob.get("username"));
+					 * bdo.put("usertype", ob.get("usertype")); ls.add(bdo); } }
+					 * getSession().put("binds", ls); }
+					 */
+
+					DBObject user = MongoUtil.getInstance().getDB()
+							.getCollection("weixinuser")
+							.findOne(new BasicDBObject("openid", openid));
+					if (!CommonUtil.isEmpty(user))
 						getSession().put("nickname", user.get("nickname"));
 
 				} else {
@@ -137,7 +134,7 @@ public class WeixinLoginAction extends GuestActionBase {
 			} catch (Exception e) {
 				log.error(e);
 			}
-			try {				
+			try {
 				org.apache.struts2.ServletActionContext.getResponse()
 						.sendRedirect(redir);
 			} catch (Exception e) {
@@ -177,7 +174,5 @@ public class WeixinLoginAction extends GuestActionBase {
 	public void setState(String state) {
 		this.state = state;
 	}
-	
-	
-	
+
 }
